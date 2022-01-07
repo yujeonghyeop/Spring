bf1f79a65856a38093b00ac114e68b9a2e66710d
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-01-07 18:19:58 +0900

    spring_3

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,23 @@ public class MemberController {
     @Autowired      //스프링 컨테이너에서 멤버서비스를 가져온고, dependenct Injection 의존관계를 주입해준다. controller와 memeberService가 연결됨.
     public MemberController(MemberService memberService) {  //memberService는 순수 자바 클래스이기 때문에 스프링이 알 방법이 없다.
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
